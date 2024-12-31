cd8eb40deeb3bd9bcb530cc31c353031164f7990
Nicolas Torres <111529835+Thowers@users.noreply.github.com>
2024-12-31 18:18:56 -0500

    Add files via upload

---
+++ src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
@@ -0,0 +1,16 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public record DatosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") int totalTemporadas,
+                         @JsonAlias("imdbRating") String evaluacion,
+                         @JsonAlias("Genre") String genero,
+                         @JsonAlias("Plot") String sinopsis,
+                         @JsonAlias("Poster") String poster,
+                         @JsonAlias("Actors") String actores)
+{
+}
